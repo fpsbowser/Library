3f330856751c0818911c79c2b4b3bb06396c29ba
Dakota <prattdakota12@gmail.com>
2022-05-16 18:07:41 -0400

    Added form validation

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Refactor to class
 class Book {
-    constructor(title, author, pages) {
-        this.title = title
-        this.author = author
-        this.pages = pages
-    }
+  constructor(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+  }
 }
 
 // function Book(title, author, pages, read) {
@@ -14,126 +14,135 @@ class Book {
 //     this.read = read
 // }
 
-const book = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'read')
+const book = new Book("The Hobbit", "J.R.R. Tolkien", "295", "read");
 
 let i = 0;
 let myLibrary = [book];
-let bookTitles = ['thehobbit'];
+let bookTitles = ["thehobbit"];
 loopArray(book.title, book.author, book.pages, book.read);
 
-const modal = document.getElementById('modal');
-const form = document.getElementById('modal-form');
-const formSubmit = document.getElementById('submit');
+const modal = document.getElementById("modal");
+const form = document.getElementById("modal-form");
+const formSubmit = document.getElementById("submit");
+const title = document.getElementById("title");
 
-form.addEventListener('submit', () => {
-    let formTitle = form.elements['title'].value
-    let formAuthor = form.elements['author'].value
-    let formPages = form.elements['pages'].value
-    let formRead = form.elements['read'].value
-    addBookToLibrary(formTitle, formAuthor, formPages, formRead);
-    form.reset();
+document.addEventListener("DOMContentLoaded", function () {
+  let elements = document.getElementsByTagName("input");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].oninvalid = function (e) {
+      e.target.setCustomValidity("");
+      if (!e.target.validity.valid) {
+        e.target.setCustomValidity("This field can't be left blank");
+      }
+    };
+    elements[i].oninput = function (e) {
+      e.target.setCustomValidity("");
+    };
+  }
 });
 
-const addButton = document.getElementById('addBtn');
-addButton.addEventListener('click', () => {
-    modal.showModal();
+form.addEventListener("submit", () => {
+  let formTitle = form.elements["title"].value;
+  let formAuthor = form.elements["author"].value;
+  let formPages = form.elements["pages"].value;
+  let formRead = form.elements["read"].value;
+  addBookToLibrary(formTitle, formAuthor, formPages, formRead);
+  form.reset();
 });
 
-const closeButton = document.getElementById('closebtn');
-closeButton.addEventListener('click', () => {
-    modal.close();
+const addButton = document.getElementById("addBtn");
+addButton.addEventListener("click", () => {
+  modal.showModal();
 });
 
+const closeButton = document.getElementById("closebtn");
+closeButton.addEventListener("click", () => {
+  modal.close();
+});
 
-
-
-
-
-// FUNCTIONS 
+// FUNCTIONS
 
 // Function that loops through the myLibrary array and displays each book on the webpage.
 
 function loopArray(arg1, arg2, arg3) {
-    for (i; i < myLibrary.length; i++) {
-        addCard(arg1, arg2, arg3)
-    }
+  for (i; i < myLibrary.length; i++) {
+    addCard(arg1, arg2, arg3);
+  }
 }
 
 // Function that creates Element and applies style to it
 
 function addCard(cardtitle, cardauthor, cardpages) {
-    const currentDiv = document.getElementById('card-container-id')
-    const newCard = document.createElement('div');
-    newCard.className = 'card';
-    currentDiv.appendChild(newCard);
+  const currentDiv = document.getElementById("card-container-id");
+  const newCard = document.createElement("div");
+  newCard.className = "card";
+  currentDiv.appendChild(newCard);
 
-    const bookTitle = document.createElement('p');
-    bookTitle.innerHTML = cardtitle;
-    bookTitle.className = 'title';
-    newCard.appendChild(bookTitle);
+  const bookTitle = document.createElement("p");
+  bookTitle.innerHTML = cardtitle;
+  bookTitle.className = "title";
+  newCard.appendChild(bookTitle);
 
-    const bookAuthor = document.createElement('p');
-    bookAuthor.innerHTML = cardauthor;
-    bookAuthor.className = 'author';
-    newCard.appendChild(bookAuthor);
+  const bookAuthor = document.createElement("p");
+  bookAuthor.innerHTML = cardauthor;
+  bookAuthor.className = "author";
+  newCard.appendChild(bookAuthor);
 
-    const bookPages = document.createElement('p');
-    bookPages.innerHTML = `${cardpages} pages`;
-    bookPages.className = 'pages';
-    newCard.appendChild(bookPages);
+  const bookPages = document.createElement("p");
+  bookPages.innerHTML = `${cardpages} pages`;
+  bookPages.className = "pages";
+  newCard.appendChild(bookPages);
 
-    const bookReadBtn = document.createElement('button');
-    bookReadBtn.innerHTML = 'Read';
-    bookReadBtn.className = 'readBtn';
-    bookReadBtn.setAttribute('id', `readbutton${i}`);
-    bookReadBtn.addEventListener('click', (e) => {
-        console.log(e.target.parentElement.ch)
-        if (e.target.innerHTML == 'Read') {
-            e.target.innerHTML = 'Not read';
-            e.target.style.backgroundColor = 'red'
-        } else {
-            e.target.innerHTML = 'Read';
-            e.target.style.backgroundColor = 'green'
-        }
-    })
-    newCard.appendChild(bookReadBtn);
+  const bookReadBtn = document.createElement("button");
+  bookReadBtn.innerHTML = "Read";
+  bookReadBtn.className = "readBtn";
+  bookReadBtn.setAttribute("id", `readbutton${i}`);
+  bookReadBtn.addEventListener("click", (e) => {
+    console.log(e.target.parentElement.ch);
+    if (e.target.innerHTML == "Read") {
+      e.target.innerHTML = "Not read";
+      e.target.style.backgroundColor = "red";
+    } else {
+      e.target.innerHTML = "Read";
+      e.target.style.backgroundColor = "green";
+    }
+  });
+  newCard.appendChild(bookReadBtn);
 
-    const removeButton = document.createElement('button');
-    removeButton.innerHTML= 'Remove';
-    removeButton.className = 'removeBtn';
-    removeButton.addEventListener('click', (e) => {
-        cardtitle = cardtitle.replace(/\s+/g, '');
-        newCard.id = cardtitle.toLowerCase();
-        e.target.parentElement.remove();
-        let index = bookTitles.indexOf(cardtitle.toLowerCase().replace(/\s+/g, ''));
-        bookTitles.splice(index, 1);
-        myLibrary.splice(index, 1);
-        i -= 1;
-    })
-    newCard.appendChild(removeButton);
-    
+  const removeButton = document.createElement("button");
+  removeButton.innerHTML = "Remove";
+  removeButton.className = "removeBtn";
+  removeButton.addEventListener("click", (e) => {
+    cardtitle = cardtitle.replace(/\s+/g, "");
+    newCard.id = cardtitle.toLowerCase();
+    e.target.parentElement.remove();
+    let index = bookTitles.indexOf(cardtitle.toLowerCase().replace(/\s+/g, ""));
+    bookTitles.splice(index, 1);
+    myLibrary.splice(index, 1);
+    i -= 1;
+  });
+  newCard.appendChild(removeButton);
 }
 
 // Function to add book to myLibrary array
 
 function addBookToLibrary(one, two, three, four) {
-    const newBook = new Book(one, two, three, four);
+  const newBook = new Book(one, two, three, four);
 
-    if (bookTitles.includes(one.toLowerCase())) {
-        alert('Book already exists!')
-    } else {
-        myLibrary.push(newBook)
-        bookTitles.push(one.toLowerCase().replace(/\s+/g, ''));
-        loopArray(one, two, three, four);
-    }
-    newBook.status(four);
+  if (bookTitles.includes(one.toLowerCase())) {
+    alert("Book already exists!");
+  } else {
+    myLibrary.push(newBook);
+    bookTitles.push(one.toLowerCase().replace(/\s+/g, ""));
+    loopArray(one, two, three, four);
+  }
+  newBook.status(four);
 }
 
 Book.prototype.status = (read) => {
-    let correctButton = `readbutton${i - 1}`;
-    if (read === 'notread') {
-            document.getElementById(correctButton).style.backgroundColor = 'red';
-            document.getElementById(correctButton).innerHTML = 'Not read';
-
-    } 
-}
+  let correctButton = `readbutton${i - 1}`;
+  if (read === "notread") {
+    document.getElementById(correctButton).style.backgroundColor = "red";
+    document.getElementById(correctButton).innerHTML = "Not read";
+  }
+};
